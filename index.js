78c24b81dc050a1ea835ba8831c9700cc709a6bd
= <=>
2019-07-26 21:28:51 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,53 @@
+
+   	const PORT = 3000								//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
+   var express = require("express");
+	var app = express();
+	app.use(express.static("./public"));
+	var server = require("http").Server(app);
+	var io = require("socket.io")(server);
+	app.set("view engine","ejs");
+	app.set("views","./views" )
+	var ip = require("ip")
+
+						//#Phải khởi tạo io sau khi tạo app!
+    server.listen(PORT, function()
+    {
+    	console.log("connected")
+
+    });										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
+    
+    app.get("/", function(req , res)
+    {
+    	res.render("trangchu")
+    })
+
+    io.on("connection",function(socket)
+    {
+        console.log(ip.address())
+        console.log("Có người kết nối")
+        socket.on("CLIENT-SEND-MODE-RAIN",function()
+        {
+            io.sockets.emit("SERVER-SEND-RAIN-MODE", {MODE:"Rain"})
+        });
+        socket.on("CLIENT-SEND-MODE-LOVE", function()
+        {
+            io.sockets.emit("SERVER-SEND-LOVE-MODE",{MODE:"Love"})
+        });
+        socket.on("CLIENT-SEND-MODE-PLAN", function()
+        {
+            io.sockets.emit("SERVER-SEND-PLAN-MODE",{MODE:"Plan"})
+        });
+        socket.on("CLIENT-SEND-MODE-DOMINO", function()
+        {
+            io.sockets.emit("SERVER-SEND-DOMINO-MODE",{MODE:"Domino"})
+        });
+         socket.on("CLIENT-SEND-MODE-OFF", function()
+        {
+            io.sockets.emit("SERVER-SEND-OFF-MODE",{MODE:"Off"})
+        });
+        socket.on("CLIENT-SEND-MODE-LAMP", function()
+        {
+            io.sockets.emit("SERVER-SEND-LAMP-MODE",{MODE:"Lamp"})
+        });
+
+    })
---
+++ public/xuly.js
@@ -0,0 +1,72 @@
+var socket = io("http://localhost:3000")
+
+$(document).ready(function()
+{
+	$("#btnModeRain").click(function()
+	{
+		socket.emit("CLIENT-SEND-MODE-RAIN")
+	})
+	$("#btnModeLove").click(function()
+	{
+		socket.emit("CLIENT-SEND-MODE-LOVE")
+	})
+	$("#btnModePlan").click(function()
+	{
+		socket.emit("CLIENT-SEND-MODE-PLAN")
+	})
+	$("#btnModeDomino").click(function()
+	{
+		socket.emit("CLIENT-SEND-MODE-DOMINO")
+	})
+	$("#btnOff").click(function()
+	{
+		socket.emit("CLIENT-SEND-MODE-OFF")
+	})
+	$("#btnLamp").click(function()
+	{
+		socket.emit("CLIENT-SEND-MODE-LAMP")
+	})
+
+
+})
+
+socket.on("SERVER-SEND-RAIN-MODE", function(data)
+{
+	alert("Đã chọn hiệu ứng mưa")
+	$("#boxMode").html("")
+	$("#boxMode").html(data.MODE)
+})
+socket.on("SERVER-SEND-LOVE-MODE", function(data)
+{
+	alert("Đã chọn hiệu ứng love")
+	$("#boxMode").html("")
+	$("#boxMode").html(data.MODE)
+})
+socket.on("SERVER-SEND-PLAN-MODE", function(data)
+{
+	alert("Đã chọn hiệu ứng plan")
+	$("#boxMode").html("")
+	$("#boxMode").html(data.MODE)
+})
+
+socket.on("SERVER-SEND-DOMINO-MODE", function(data)
+{
+	alert("Đã chọn hiệu ứng domino")
+	$("#boxMode").html("")
+	$("#boxMode").html(data.MODE)
+})
+socket.on("SERVER-SEND-OFF-MODE", function(data)
+{
+	alert("Đã tắt tất cả cả đèn")
+	$("#boxMode").html("")
+	$("#boxMode").html(data.MODE)
+})
+
+socket.on("SERVER-SEND-LAMP-MODE", function(data)
+{
+	alert("Đã chọn hiệu ứng đèn ngủ")
+	$("#boxMode").html("")
+	$("#boxMode").html(data.MODE)
+})
+
+
