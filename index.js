63102792d9c184904e9de926d51819fe8ef0b168
= <=>
2019-07-26 21:31:49 +0700

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 	var ip = require("ip")
 
 						//#Phải khởi tạo io sau khi tạo app!
-    server.listen(PORT, function()
+    server.listen(process.env.PORT||3000, function()
     {
     	console.log("connected")
 
--- public/xuly.js
+++ public/xuly.js
@@ -1,4 +1,4 @@
-var socket = io("http://localhost:3000")
+var socket = io("https://ledcube2019.herokuapp.com")
 
 $(document).ready(function()
 {
