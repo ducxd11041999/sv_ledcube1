028ec6a3c30d83c9821d0daa21fb8683aa544a06
= <=>
2019-07-31 17:57:23 +0700

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
         			//#Phải khởi tạo io sau khi tạo app!
     server.listen(process.env.PORT||3000, function()
     {
-        alert(process.env.PORT)
+        //alert(process.env.PORT)
     	console.log("connected")
         console.log(ip.address())
     });										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
@@ -51,7 +51,6 @@
         {
 
             io.sockets.emit("SERVER-SEND-LAMP-MODE",{MODE:"Lamp", AR:"6"})
-            io.sockets.emit("send_port", port_connect)
             //console.log("con heo con đáng yêu")
 
         });
