920a4bbbc906fbc36a68d1c9c794d3136aa506e9
= <=>
2019-07-31 17:20:49 +0700

    make it better

--- index.js
+++ index.js
@@ -10,10 +10,10 @@
 	var ip = require("ip")
 
 						//#Phải khởi tạo io sau khi tạo app!
-    server.listen(process.env.PORT||3000, function()
+    server.listen(3000, function()
     {
     	console.log("connected")
-
+        console.log(ip.address())
     });										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
     
     app.get("/", function(req , res)
@@ -23,31 +23,43 @@
 
     io.on("connection",function(socket)
     {
+        console.log("đã nhảy dô đây")
         console.log(ip.address())
         console.log("Có người kết nối")
         socket.on("CLIENT-SEND-MODE-RAIN",function()
         {
-            io.sockets.emit("SERVER-SEND-RAIN-MODE", {MODE:"Rain"})
+            io.sockets.emit("SERVER-SEND-RAIN-MODE", {MODE:"Rain",AR:"1"})
         });
         socket.on("CLIENT-SEND-MODE-LOVE", function()
         {
-            io.sockets.emit("SERVER-SEND-LOVE-MODE",{MODE:"Love"})
+            io.sockets.emit("SERVER-SEND-LOVE-MODE",{MODE:"Love",AR:"2"})
         });
         socket.on("CLIENT-SEND-MODE-PLAN", function()
         {
-            io.sockets.emit("SERVER-SEND-PLAN-MODE",{MODE:"Plan"})
+            io.sockets.emit("SERVER-SEND-PLAN-MODE",{MODE:"Plan",AR:"3"})
         });
         socket.on("CLIENT-SEND-MODE-DOMINO", function()
         {
-            io.sockets.emit("SERVER-SEND-DOMINO-MODE",{MODE:"Domino"})
+            io.sockets.emit("SERVER-SEND-DOMINO-MODE",{MODE:"Domino",AR:"4"})
         });
          socket.on("CLIENT-SEND-MODE-OFF", function()
         {
-            io.sockets.emit("SERVER-SEND-OFF-MODE",{MODE:"Off"})
+            io.sockets.emit("SERVER-SEND-OFF-MODE",{MODE:"Off",AR:"5"})
         });
         socket.on("CLIENT-SEND-MODE-LAMP", function()
         {
-            io.sockets.emit("SERVER-SEND-LAMP-MODE",{MODE:"Lamp"})
+
+            io.sockets.emit("SERVER-SEND-LAMP-MODE",{MODE:"Lamp", AR:"6"})
+            //console.log("con heo con đáng yêu")
+
         });
+       
 
     })
+function ParseJson(jsondata) {
+    try {
+        return JSON.parse(jsondata);
+    } catch (error) {
+        return null;
+    }
+}
